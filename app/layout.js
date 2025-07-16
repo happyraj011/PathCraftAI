faf811df66503bc2fe557524008bf18da090ad63
happy <happyraj9939@gmail.com>
2025-07-17 00:48:10 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,23 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+         <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header/>
+            <main className="min-h-screen">
+              {children}
+            </main>
+            <footer>
+              <div>
+                footer
+              </div>
+            </footer>
+            
+          </ThemeProvider>
       </body>
     </html>
   );
